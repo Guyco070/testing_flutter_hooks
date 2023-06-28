8d908a87b15d6497ed0857f5873aff744c30c91a
Guy Cohen <gaico070@gmail.com>
2023-06-28 14:16:25 +0300

    Step-3 - useMemoized + useFuture + CompactMap

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform
+  ]) => map(
+    transform ?? (e) => e,
+  ).where((element) => element != null).cast();
+}
+
+void testIt() {
+  final values = [1, 2, null, 3,];
+  print(CompactMap(values).compactMap()); // non null values
+  print(CompactMap(values).compactMap((e) => e == null ? null : "$e YEHHH")); // non null values + replace values
 }
 
 class MyApp extends StatelessWidget {
@@ -19,10 +35,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Stream<String> getTime() => Stream.periodic(
-  const Duration(seconds: 1),
-  (_) => DateTime.now().toIso8601String(),
-);
+const url = "https://my.alfred.edu/zoom/_images/foster-lake.jpg";
 
 class HomePage extends HookWidget {
   const HomePage({super.key,});
@@ -30,13 +43,36 @@ class HomePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
 
-    final dateTime = useStream(getTime());
+    // // testing the compactMap
+    // testIt();
+
+    // // will load the image then call build function again 
+    // final image = useFuture(
+    //   NetworkAssetBundle(Uri.parse(url))
+    //   .load(url)
+    //   .then((data) => data.buffer.asUint8List())
+    //   .then((data) => Image.memory(data))
+    // );
+
+    // to avoid rebuilding on every load of the image (ignoring second build)
+    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
+      .load(url)
+      .then((data) => data.buffer.asUint8List())
+      .then((data) => Image.memory(data))
+    );
+
+    final snapshot = useFuture(future);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(dateTime.data ?? "Home Page"),
+        title: const Text('Home Page'),
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          // image.data // can be null
+          snapshot.data
+        ].compactMap().toList(),
+      )
     );
   }
 }
