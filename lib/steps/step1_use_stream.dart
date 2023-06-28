1658fd6eadb8bcc739d8524c0fea3b7c7bb47ce7
Guy Cohen <gaico070@gmail.com>
2023-06-28 14:43:38 +0300

    Step 1 - useStream

--- lib/steps/step1_use_stream.dart
+++ lib/steps/step1_use_stream.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
-      home: const HomePage(),
-    );
-  }
-}
-
 Stream<String> getTime() => Stream.periodic(
   const Duration(seconds: 1),
   (_) => DateTime.now().toIso8601String(),
